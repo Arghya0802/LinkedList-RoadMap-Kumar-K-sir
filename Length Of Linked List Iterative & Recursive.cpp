fc48a553c9df1a056b19d71f71217c2bc660a6ee
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2022-12-20 23:26:21 +0530

    Create Length Of Linked List Iterative & Recursive.cpp

---
+++ Length Of Linked List Iterative & Recursive.cpp
@@ -0,0 +1,34 @@
+// Recurisve function to count Nodes in a LinkedList
+
+int countLinkedListNodes(Node *p)
+{
+        if(p == NULL) // Base Case
+            return 0 ;
+            
+        return 1 + countLinkedListNodes(p -> next) ;
+}
+
+
+int getCount(struct Node* head)
+{
+        int ans = 0 ; 
+        
+        Node * p = head ;
+        
+        while(p != NULL) // We keep on counting all the nodes until P becomes NULL
+        {
+            ans += 1 ;
+            
+            p = p -> next ;
+        }
+        
+        return ans ;
+}
+
+// For the Iterative Code
+// Time Complexity:  O(N)
+// Space Complexity: O(1)
+
+// For the Recurisve Code
+// Time Complexity:  O(N)
+// Space Complexity: O(N) [Recursive Stack Space]
